8c999e70c54db0cfb9f0f48eb0e667b5c2bbea39
ShariqAli-Dev <shariqali.dev@gmail.com>
2021-10-24 21:19:04 -0400

    get /projects/

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,21 @@
-// build your `/api/projects` router here
+const express = require('express');
+const Projects = require('./model');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  try {
+    const projects = await Projects.getAll();
+    const projectsToBool = projects.map((project) => {
+      return {
+        ...project,
+        project_completed: Boolean(project.project_completed),
+      };
+    });
+    res.status.json(projectsToBool);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const projectRouter = require('./project/router');
-const resourceRouter = require('./resource/router');
-const taskRouter = require('./task/router');
+// const resourceRouter = require('./resource/router');
+// const taskRouter = require('./task/router');
 
 const server = express();
 
 server.use(express.json());
 server.use('/api/projects', projectRouter);
-server.use('/api/resources', resourceRouter);
-server.use('/api/tasks', taskRouter);
+// server.use('/api/resources', resourceRouter);
+// server.use('/api/tasks', taskRouter);
 
 server.use((err, req, res, next) => {
   res.status(err.status || 500).json({
     message: err.message,
   });
 });
+
+module.exports = server;
